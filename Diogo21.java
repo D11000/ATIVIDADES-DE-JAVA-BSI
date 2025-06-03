9a9897cea7566704cf5ac55e7d91b9738ebcb605
D11000 <118454742+D11000@users.noreply.github.com>
2025-06-03 08:36:24 -0300

    LIsta 2

--- Diogo21.java
+++ Diogo21.java
@@ -1,2 +1,26 @@
+import java.util.Scanner;
+
+
 public class Diogo21 {
+    public static void main(String[] args)
+    {
+        Scanner ent = new Scanner(System.in);
+
+        System.out.println("Digite a medida do angulo: ");
+        double Angulo = ent.nextDouble();
+
+        System.out.println("Digite a altura da parede: ");
+        double Altura = ent.nextDouble();
+
+        double Pi = 3.14;
+        double seno = Math.sin(Angulo);
+        double Convradiano = (Pi*seno)/180;
+
+        double Medida = Altura/Convradiano;
+
+        System.out.format("A medida da escada é igual a : %.2f", Medida);
+
+
+
+    }
 }
