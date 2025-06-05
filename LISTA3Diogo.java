9277dfb4c321d50c6bd28dd42c7e95bbf4dae033
D11000 <118454742+D11000@users.noreply.github.com>
2025-06-05 07:59:32 -0300

    LISTA 3

---
+++ LISTA3Diogo.java
@@ -0,0 +1,44 @@
+//1. Faça um algoritmo que receba a idade de um nadador e mostre a categoria do mesmo. Faça os testes
+//necessários em relação a idade inválida.
+//Categoria Operação
+//Infantil 5 a 7 (inclusive)
+//Juvenil 8 a 10 (inclusive)
+//Adolescente 11 a 15 (inclusive)
+//Adulto 16 a 40 (inclusive)
+//Sênior Acima de 40 (inclusive)
+
+
+import java.util.Scanner;
+public class LISTA3Diogo {
+    public static void main(String[]args){
+        Scanner Ent = new Scanner(System.in);
+        System.out.println("Digite sua idade");
+
+        int idade=  Ent.nextInt() ;
+        if (idade >= 4 && idade <=7 ){
+            System.out.println("Categoria infantil");
+        }
+        if (idade >= 8 && idade <=10 ){
+            System.out.println("Categoria juvenil");
+
+
+        }
+        if (idade >= 11 && idade <=15 ){
+            System.out.println("Categoria adolescente");
+
+        }
+        if (idade >= 16 && idade <=40 ){
+            System.out.println("Categoria adulto");
+
+        }
+        if (idade >= 40 ){
+            System.out.println("Categoria Senior");
+
+
+
+
+        }
+    }
+
+
+}
