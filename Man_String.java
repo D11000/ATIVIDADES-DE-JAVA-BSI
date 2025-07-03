2557fa8766bf5dc1cb2c74e068bac2bc8ae61fb0
D11000 <118454742+D11000@users.noreply.github.com>
2025-07-03 08:44:43 -0300

    Manipulação de Strings 1.0
    Versão de base com instruções apenas.

---
+++ Man_String.java
@@ -0,0 +1,17 @@
+//Realize o cadastro de um usuario com as seguintes informações
+//- Nome:Deve possuir mais de 2 caracteres
+//- Idade : Deve estar entre 0 e 150
+//- Email: Deve possuir o caracter arroba e ".com"
+//- Senha : Deva conter número, letra maiscula, letra minuscula e caracter especial('#','*','_','!','@')
+//Utilize as funções da classe Character
+//-isUpperCase
+//-isLowerCase
+//isDigit
+//---------------------------------------------------------------//
+import java.util.Scanner;
+
+public class Man_String {
+    public static void main(String[] args) {
+
+    }
+}
