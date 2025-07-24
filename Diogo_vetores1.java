dbf06ae6e3a2a147fd466267b4afd63415cbeb79
D11000 <118454742+D11000@users.noreply.github.com>
2025-07-24 08:32:49 -0300

    1

--- Diogo_vetores1.java
+++ Diogo_vetores1.java
@@ -9,20 +9,62 @@ import java.util.Scanner;
 public class Diogo_vetores1 {
     public static void main(String[] args) {
      Scanner in = new Scanner(System.in);
-     int idade[]= new int[5];
+     int idade[]= new int[40];
+     double media=0, media_final=0;
+     int aluno_velho=0  ;
+     int aluno_novo=0 ;
+     int maior16=0;
+     int menor16=0;
+
+
 
          for ( int i = 1 ; i <idade.length ; i++){
              System.out.println("Digite a idade ");
 
              idade[i]= in.nextInt();
 
-             if (idade[i] <= 16){
-                 System.out.println(idade[i]);
+
+             if(i ==1 ){
+                 aluno_velho= idade[i];
+                 aluno_novo= idade[i];
+
+
 
              }
+             if(idade[i] <= 16 ){
+                 menor16++;
+
+
+
+             }
+             if(idade[i] >  16 ){
+                 maior16++;
+
+
+
+
+             }
+             media+= idade[i];
+
+
+
+
+
+
+
 
 
 
          }
+
+        System.out.println("O maior valor "+Math.max(idade[4],aluno_novo));
+        System.out.println("O maior valor "+Math.min(aluno_velho,idade[4]));
+        System.out.println(" A quantidade maior de 16 é "+maior16);
+        System.out.println(" A quantidade menor de 16 é "+menor16);
+
+
+        media_final= media/4;
+        System.out.println("A media é "+ media_final);
+
     }
 }
